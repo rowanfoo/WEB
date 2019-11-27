8c993bcce144981654d2cfd3e9de013a32203a9b
rowanfoo <rowanfoo@gmail.com>
2019-11-28 07:56:37 +0800

    first commit

---
+++ src/test/kotlin/com/dharmma/webserver/WebserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dharmma.webserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebserverApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
