5c0ea96fc742f033deaa91017bb1276039bd7abc
rowanfoo <rowanfoo@gmail.com>
2019-12-05 08:22:24 +0800

    Refactor to have WishList

---
+++ src/main/resources/static/app-service/Utility.js
@@ -0,0 +1,29 @@
+angular
+    .module('app')
+    .factory('Utility', Service);
+
+function Service($http, $localStorage) {
+    var service = {};
+
+    service.Comma = comma;
+    return service;
+
+    function comma(data) {
+        var code = '';
+        console.log('------------Service -----commat----- ' + data.code);
+        // for (i in data.data) {
+        //
+        // }
+        var x;
+        for (x of data) {
+
+            code = code + ',' + x;
+            console.log('------------Service -----commat----- ' + code);
+        }
+
+        code = code.substring(1, code.length);
+        return code;
+    }
+}
+
+
