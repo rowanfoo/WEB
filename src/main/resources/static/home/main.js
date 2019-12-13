e11dd3dfc466c7ff962e43673b8e9e0da642384a
rowanfoo <rowanfoo@gmail.com>
2019-12-13 10:22:16 +0800

    cleanup ,

--- src/main/resources/static/home/main.js
+++ src/main/resources/static/home/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     $('#example').DataTable();
 });
 var module = angular.module('app');
-
 module.controller('sideNavController', Controller);
 
 
@@ -14,41 +13,15 @@ function Controller($scope, $rootScope, $location, $http,User) {
     $scope.algos = [];
 
     $scope.golink = function (type, id) {
-        console.log('---goto path----');
-        console.log('---goto path----' + type + "---------" + id);
         var url = 'main/viewlist/' + type + '/' + id;
-        console.log('---goto path----' + type + "---------" + url);
-
         $location.path(url);
     };
 
-    $scope.golink2 = function (type, id) {
-        console.log('---goto path----' + type + "-------------" + id);
+
+    $scope.gofav = function () {
+           $location.path('/wishlist');
     };
-    // $scope.mychartbutton = function () {
-    //     console.log('-----sideNavController--------viewController -GOING ROUTE------1');
-    //     $location.path("main/viewchart");
-    //
-    // }
     User.GetUserAlgo('rowan').then(function (data) {
         $scope.algos  = data;
     });
-
-    // $http.get('http://localhost:8090/items').then(function (data) {
-    //     var mydata = data.data;
-    //     console.log("hello world--------" + mydata);
-    //     console.log("hello world--------" + mydata.length);
-    //     console.log(JSON.stringify(mydata, null, "    "));
-    //     $rootScope.algoscope = [];
-    //
-    //     mydata.forEach(function (obj) {
-    //         console.log(obj.id);
-    //         $rootScope.algoscope.push(obj.value);
-    //         $scope.algos.push({id: obj.id, value: obj.value});
-    //     });
-
-
-    // });
-
-
 };
