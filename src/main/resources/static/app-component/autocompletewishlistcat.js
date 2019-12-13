5b57889ea27c5a53f906ef80fd49d8b5aa8d5325
rowanfoo <rowanfoo@gmail.com>
2019-12-13 10:19:14 +0800

    cleanup

--- src/main/resources/static/app-component/autocompletewishlistcat.js
+++ src/main/resources/static/app-component/autocompletewishlistcat.js
@@ -4,59 +4,24 @@ angular.module('app').component('wishlistcat', {
     bindings: {selectedcode: '<'},
     controller: ['$scope', '$http', '$rootScope', 'WishList', function ($scope, $http, $rootScope, WishList) {
         console.log('-----------------COMPONENT wishlistcat---------------');
-        //  console.log('-----------------IN APP CONTRL 6----------------' + $rootScope.config.wishcategory);
-        //rootScope.stockallrest
-        var wishlisturl = 'http://localhost:8045/wishlistcategorys';
 
-        // $http.get(wishlisturl).then(function (data) {
-        //     console.log(JSON.stringify(data, null, "    "));
-        //
-        //     $scope.codes = [];
-        //     for (i in data.data) {
-        //         console.log('----DATA-------' + data.data[i]);
-        //         $scope.codes.push(data.data[i]);
-        //     }
-        // });
-
-
-        // $scope.codes = WishList.GetAllWishCategory();
-        // WishList.GetAllWishCategory().then(function (value) {
-        //     console.log('HERE IN CONTROLLER ----------wishlistController---WISHLIST----RESULT-------****');
-        //     //    $scope.codes = value;
-        // });
         WishList.GetAllWishCategory().then(function (value) {
             console.log('HERE IN CONTROLLER ----------wishlistController---WISHLIST----RESULT-------****');
             $scope.codes = value;
         });
-
-
-        console.log(JSON.stringify($scope.codes, null, "    "));
-
+        // console.log(JSON.stringify($scope.codes, null, "    "));
         $scope.filterItems = function (searchWord) {
-            console.log('-----------------filterItems----------------' + searchWord);
             $rootScope.selectcode = searchWord;
             var arr = [];
             $scope.codes.forEach(function (item) {
-                // if (item.toLowerCase().indexOf(searchWord.toLowerCase()) >= 0  ){
-                //     arr.push(item);
-                // }
-                //
                 arr.push(item);
             });
-
-
             return arr;
         };
 
         $scope.selectedItemChange = function (item) {
-            console.log(' SUBMIT  investigate-create  selectedItem   ----' + item);
             $scope.mycode = item.code;
-            console.log(' SUBMIT  investigate-create  selectedItem   ----' + $scope.mycode);
             $scope.selectedcode($scope.mycode);
-
         }
-
     }]//eof contrl
-
-
 });
