9c6b60f94f0ff8e3235b15bb9624032ff98336fe
rowanfoo <rowanfoo@gmail.com>
2019-12-15 09:59:31 +0800

    add configurl

--- src/main/resources/static/app-static/config.js
+++ src/main/resources/static/app-static/config.js
@@ -1,20 +1,21 @@
+var rooturl = 'http://rowanfoo.ddns.net';
+var serverurl = 'http://rowanfoo.ddns.net:10100/equity';
+var wishlisturl = 'http://rowanfoo.ddns.net:10100/wishlistserver';
+var algoturl = 'http://rowanfoo.ddns.net:10100/';
+var oauthurl = 'http://rowanfoo.ddns.net:10200/';
+var userurl = 'http://rowanfoo.ddns.net:10300/';
+var wishurl = 'http://rowanfoo.ddns.net:10400/';
+var configurl = 'http://rowanfoo.ddns.net:10500/config/';
 
-var rooturl='http://rowanfoo.ddns.net';
-var serverurl='http://rowanfoo.ddns.net:10100/equity';
-var wishlisturl='http://rowanfoo.ddns.net:10100/wishlistserver';
-var algoturl='http://rowanfoo.ddns.net:10100/';
-var oauthurl='http://rowanfoo.ddns.net:10200/';
-var userurl='http://rowanfoo.ddns.net:10300/';
-var wishurl='http://rowanfoo.ddns.net:10400/';
 
-
-function setconfig($rootScope){
+function setconfig($rootScope) {
     console.log('----setconfig-----');
-    $rootScope.config={
+    $rootScope.config = {
         algoturl: algoturl,
         oauthurl: oauthurl,
-        userurl: userurl ,
-        wishurl: wishurl
+        userurl: userurl,
+        wishurl: wishurl,
+        configurl: configurl
     };
 
 }
