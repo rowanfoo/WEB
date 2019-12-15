00c7f896ccf90753f1458772b5370517157f258d
rowanfoo <rowanfoo@gmail.com>
2019-12-15 09:58:26 +0800

    add constant data

--- src/main/resources/static/comment/dialog/commentcreate.js
+++ src/main/resources/static/comment/dialog/commentcreate.js
@@ -2,24 +2,43 @@ angular
     .module('app')
     .factory('CommentCreate', Service);
 
-function Service($http,$rootScope, $q) {
+function Service($http, $rootScope, $q, Config, $mdDialog) {
     var service = {};
     service.Createcontroller = createcontroller;
     return service;
-    function createcontroller($scope, $http, aTitle) {
-   console.log('-----------CommentCreate---------' +aTitle );
-        $scope.code = aTitle;
 
-        $scope.myFunc = function () {
-            console.log('-----------submit---------'  );
-            console.log(JSON.stringify($scope.comment, null, "    "));
+    function createcontroller($scope, $http, aTitle, $mdDialog) {
+        $scope.code = aTitle;
+        $scope.selected = 'equity';
+
+        // $scope.comment.code = aTitle;
+        // $scope.comment.userid = 'rowan';
+
+        $scope.comment = {
+            code: aTitle,
+            userid: 'rowan',
+            type: 'equity'
         }
 
 
+        Config.GetConfig('comment', '0').then(function (data) {
+            $scope.config = data
+
+            //           console.log(JSON.stringify($scope.config, null, "    "));
+        });
+
+
+        $scope.myFunc = function () {
+            console.log('-----------submit---------');
+
+            //    Config.Save($scope.comment);
+
+            console.log(JSON.stringify($scope.comment, null, "    "));
+            $mdDialog.hide();
+        }
+
 
     };
-
-
 
 
     // function getUserAlgo(user) {
