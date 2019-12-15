d3db6d1f3f12981a569363ffd34a7f94ca3e4605
rowanfoo <rowanfoo@gmail.com>
2019-12-15 10:04:01 +0800

    add comment

--- src/main/resources/static/list/view.js
+++ src/main/resources/static/list/view.js
@@ -2,7 +2,7 @@ var module = angular.module('app');
 module.controller('viewController', Controller);
 
 
-function Controller($scope, $rootScope, $http, $state, $location) {
+function Controller($scope, $rootScope, $http, $state, $location, CommentCreate, $mdDialog) {
     $scope.loading = true;
     var type = $state.params.type;
     var id = $state.params.id;
@@ -11,10 +11,27 @@ function Controller($scope, $rootScope, $http, $state, $location) {
         $location.path("main/viewchart");
     };
     $scope.chartdetail = function (code) {
-        alert('helo world  -1: '+ code);
-        $location.path("main/multichart/"+code);
+        alert('helo world  -1: ' + code);
+        $location.path("main/multichart/" + code);
 
     };
+
+
+    $scope.comment = function (code) {
+        alert('comment: ' + code);
+        $mdDialog.show({
+            //  templateUrl: "/WEB/static/dialog.html",
+            templateUrl: "/WEB/static/comment/dialog/commentDialog.html",
+            parent: angular.element(document.body),
+            clickOutsideToClose: true,
+            locals: {aTitle: code},
+            controller: function ($scope, $http, aTitle) {
+                CommentCreate.Createcontroller($scope, $http, aTitle, $mdDialog)
+            }
+        });
+    };
+
+
     $http.get($rootScope.config.algoturl + type + '/' + id + '?sector=200').then(function (data) {
         // $http.get('http://192.168.0.10:10500/' + type).then(function (data) {
         $scope.data = data.data
