e5920d162615615147c6482a6475194cfc877ff2
rowanfoo <rowanfoo@gmail.com>
2019-12-19 00:32:11 +0800

    fix commentjs component

--- src/main/resources/static/app-component/comment.js
+++ src/main/resources/static/app-component/comment.js
@@ -1,11 +1,13 @@
 //templateUrl: '/wishlistview/resources/wishlistview/route//wishlistcreate.html',
 angular.module('app').component('comment', {
     templateUrl: '/WEB/static/app-component/comment.html',
-    bindings: {code: '@', userid: '@'},
+    bindings: {code: '<', userid: '@'},
+    // bindings: {code: '@'},
     controller: ['$scope', '$http', '$rootScope', 'Comment', function ($scope, $http, $rootScope, Comment) {
-        console.log('-----------------COMPONENT Comment---------------');
-        console.log('-----------------COMPONENT Comment--------13-------' + $scope.code);
-        console.log('-----------------COMPONENT Comment--------14------' + this.code);
+        console.log('-----------------COMPONENT Comment----3-----------');
+        //     console.log('-----------------COMPONENT Comment---1-----13-------' + $scope.code);
+        //   console.log('-----------------COMPONENT Comment--------14------' + $scope.userid);
+        // console.log('-----------------COMPONENT Comment--------15------' + $scope.userid);
         // WishList.GetAllWishCategory().then(function (value) {
         //     console.log('HERE IN CONTROLLER ----------wishlistController---WISHLIST----RESULT-------****');
         //     $scope.codes = value;
@@ -24,33 +26,43 @@ angular.module('app').component('comment', {
         //     $scope.mycode = item.code;
         //     $scope.selectedcode($scope.mycode);
         // }
-        this.$onInit = function () {
-            // do all your initializations here.
-            // create a local scope object for this component only. always update that scope with bindings. and use that in views also.
-            console.log('--------anydata-----------' + this.code);
-            console.log('--------anydata-----------' + $scope.code);
-            $scope.code = this.code;
-            console.log('--------anydata*******' + $scope.code);
-
-        };
-
         $scope.showcomment = false;
         $scope.ma = [];
         $scope.comments = [];
         $scope.comments.push({value: 'hello word'});
-        $scope.comments.push({value: 'this is my contents'});
-        $scope.comments.push({value: 'this is MORE contents'});
-        $scope.comment = {value: ''};
+        // $scope.comments.push({value: 'this is my contents'});
+        // $scope.comments.push({value: 'this is MORE contents'});
+        // $scope.comment = {value: ''};
 
-        Comment.getAllCommentbyCode(userid, code).then(function (data) {
-            data.data.forEach(function (data) {
-                $scope.comments.push([
-                    {
-                        value: data.text
-                    }
-                ]);
+        this.$onInit = function () {
+            // do all your initializations here.
+            // create a local scope object for this component only. always update that scope with bindings. and use that in views also.
+            //     console.log('--------anydata-----------' + this.code);
+            //   console.log('--------anydata-----------' + this.userid);
+            // console.log('--------anydata-----------' + $scope.code);
+            $scope.code = this.code;
+            $scope.userid = this.userid;
+            // console.log('--------anydata*******' + $scope.code);
+
+            Comment.GetAllCommentbyCode($scope.userid, $scope.code).then(function (data) {
+                console.log('--------comment COMPONENT*******');
+
+                data.forEach(function (data) {
+                    $scope.comments.push(
+                        {
+                            value: data.text
+                        }
+                    );
+                });
+                console.log(JSON.stringify($scope.comments, null, "    "));
             });
-        });
+
+
+        };
+
+
+        //   console.log('-----------------COMPONENT Comment---1-----15-------' + $scope.code);
+        // console.log('-----------------COMPONENT Comment--------16------' + $scope.userid);
 
 
         $scope.newItem = function () {
