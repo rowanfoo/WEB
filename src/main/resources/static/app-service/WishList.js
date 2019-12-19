c75fa80dd909ba626645f9a358f69a63a561d237
rowanfoo <rowanfoo@gmail.com>
2019-12-19 09:21:57 +0800

    add wish details

--- src/main/resources/static/app-service/WishList.js
+++ src/main/resources/static/app-service/WishList.js
@@ -2,20 +2,24 @@ angular
     .module('app')
     .factory('WishList', Service);
 
-function Service($rootScope , $http, $localStorage, $q) {
+function Service($rootScope, $http, $localStorage, $q) {
     var service = {};
     service.GetAllWishCategory = getAllWishCategory;
     service.GetWishCodes = getWishCodes;
+    service.GetWishDetails = getWishDetails;
+
+
     return service;
+
     //var wishlisturl = $rootScope.config.wishurl;
 
     function getAllWishCategory() {
         var wishlisturl = $rootScope.config.wishurl;
 //        var wishlisturl = 'http://localhost:8045/wishlistcategorys';
-        console.log('---RUN-   getAllWishCategory-----1--');
-        console.log('---RUN-   getAllWishCategory-----1--'+wishlisturl);
-        wishlisturl=wishlisturl+'wishlistcategorys';
-        console.log('---RUN-   getAllWishCategory-------'+ wishlisturl);
+        // console.log('---RUN-   getAllWishCategory-----1--');
+        // console.log('---RUN-   getAllWishCategory-----1--'+wishlisturl);
+        wishlisturl = wishlisturl + 'wishlistcategorys';
+        // console.log('---RUN-   getAllWishCategory-------'+ wishlisturl);
         var q = $q.defer();
         var seriesOptions = [];
         $http.get(wishlisturl).then(function (data) {
@@ -29,8 +33,9 @@ function Service($rootScope , $http, $localStorage, $q) {
 
     function getWishCodes(category) {
         var wishlisturl = $rootScope.config.wishurl;
-       // var wishlisturl = 'http://localhost:8045/wishcategorycodes';
-        wishlisturl=wishlisturl+'wishcategorycodes';
+
+        // var wishlisturl = 'http://localhost:8045/wishcategorycodes';
+        wishlisturl = wishlisturl + 'wishcategorycodes';
         var code = '';
         var q = $q.defer();
         $http.get(wishlisturl + '?category=' + category).then(function (data) {
@@ -40,6 +45,17 @@ function Service($rootScope , $http, $localStorage, $q) {
         return q.promise;
     }
 
+
+    function getWishDetails(codes) {
+        var wishlisturl = $rootScope.config.algoturl + '/wishlist/metadata/' + codes;
+        console.log('---RUN-   getWishDetails-----1--' + wishlisturl);
+        var q = $q.defer();
+        $http.get(wishlisturl).then(function (data) {
+            console.log(JSON.stringify(data, null, "    "));
+            q.resolve(data.data);
+        });
+        return q.promise;
+    }
 
 }
 
