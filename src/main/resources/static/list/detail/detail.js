bfb8dedfa8ceae1f682b9411fa444bafe56ef178
rowanfoo <rowanfoo@gmail.com>
2019-12-19 09:22:16 +0800

    cleanup

--- src/main/resources/static/list/detail/detail.js
+++ src/main/resources/static/list/detail/detail.js
@@ -7,9 +7,6 @@ function Controller($scope, $rootScope, $http, $state, $location, CommentCreate,
     //  $scope.loading = true;
     var code = $state.params.code;
     $scope.mycode = code;
-    console.log('------viewDetailController--------------' + code);
-    console.log('------viewDetailController--------------' + $scope.mycode);
-
     Fundamental.GetFundamentalCode(code).then(function (data) {
 
         // console.log(JSON.stringify(data, null, "    "));
--- src/main/resources/static/wishlist/detail/view.js
+++ src/main/resources/static/wishlist/detail/view.js
@@ -2,7 +2,7 @@ var module = angular.module('app');
 module.controller('wishlistDetailController', Controller);
 
 
-function Controller($scope, $rootScope, $http, $state, $location,WishList) {
+function Controller($scope, $rootScope, $http, $state, $location, WishList) {
 
     console.log('----------wishlistDetailController -HERE IN CONTROLLER----------viewController---v1');
     $scope.loading = true;
@@ -11,12 +11,10 @@ function Controller($scope, $rootScope, $http, $state, $location,WishList) {
     var codes = $state.params.codes;
     console.log('----------viewController -HERE IN CONTROLLER----------CODES-' + codes);
     WishList.GetWishDetails(codes).then(function (data) {
-        $scope.data = data.data;
+        $scope.data = data;
+        $scope.loading = false;
     })
     // console.log('-----------HERE INviewController  CONTROLLER-----------' + type + '----------' + id);
-
-    $scope.loading = false;
-
 
 
 };
