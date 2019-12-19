76d0168afe8600d955799850dbf9e36bb6232e0b
rowanfoo <rowanfoo@gmail.com>
2019-12-20 04:53:16 +0800

    add new user url

--- src/main/resources/static/app-service/user.js
+++ src/main/resources/static/app-service/user.js
@@ -2,18 +2,22 @@ angular
     .module('app')
     .factory('User', Service);
 
-function Service($http,$rootScope, $localStorage, $q) {
+function Service($http, $rootScope, $localStorage, $q) {
     var service = {};
     service.GetUserAlgo = getUserAlgo;
     return service;
 
     function getUserAlgo(user) {
+        console.log('-----------getUserAlgo-----------------');
+
         var wishlisturl = $rootScope.config.userurl
         var q = $q.defer();
         var algoscope = [];
-        $rootScope.algoscope=[];
-        var wishlisturl =  wishlisturl + 'user/rowanfoo';
+        $rootScope.algoscope = [];
+        var wishlisturl = wishlisturl + 'user/algo/rowan';
         $http.get(wishlisturl).then(function (data) {
+            console.log(JSON.stringify(data, null, "    "));
+
             var mydata = data.data;
             mydata.forEach(function (obj) {
                 algoscope.push({id: obj.id, value: obj.value});
