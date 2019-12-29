1d6ca5e37387c4baa5b77a3308ba0b49dffaf642
rowanfoo <rowanfoo@gmail.com>
2019-12-29 23:17:48 +0800

    add login

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -115,12 +115,12 @@ module.run(function ($rootScope, $http, $location, $localStorage) {
 //     }
     //$location.path("/viewchart");
 //    console.log('---ROUTE------------1----');
-    // $location.path('/login');
+    $location.path('/login');
 //    $location.path('/wishlist');
 //    $location.path('/wishlist/wishcreate');
 //     $location.path('/wishlist-create');
 
-    $location.path('/main');
+//    $location.path('/main');
 
 //    $location.path('/main/listchart/BHP.AX,RIO.AX');
 
