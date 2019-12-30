f8abb9972979a5d900c6f678e15b071b4b56bdea
rowanfoo <rowanfoo@gmail.com>
2019-12-30 23:59:38 +0800

    fix bug

--- src/main/resources/static/app-component/autocompletewishlistcat.js
+++ src/main/resources/static/app-component/autocompletewishlistcat.js
@@ -1,11 +1,13 @@
 angular.module('app').component('wishlistcat', {
     templateUrl: '/WEB/static/app-component/autocompletewishlistcat.html',
     bindings: {selectedcode: '<'},
-    controller: ['$scope', '$rootScope', , function ($scope, $rootScope) {
+    controller: ['$scope', '$rootScope', 'WishList', function ($scope, $rootScope, WishList) {
         WishList.GetAllWishCategory().then(function (value) {
             $scope.codes = value;
+            console.log(JSON.stringify($scope.codes, null, "    "));
+
         });
-        // console.log(JSON.stringify($scope.codes, null, "    "));
+
         $scope.filterItems = function (searchWord) {
             $rootScope.selectcode = searchWord;
             var arr = [];
