0b02db8a644345b79c104dc4e6e2f9c19999f8a5
rowanfoo <rowanfoo@gmail.com>
2020-01-01 21:50:43 +0800

    add news

--- src/main/resources/static/news/news.js
+++ src/main/resources/static/news/news.js
@@ -2,12 +2,18 @@ var module = angular.module('app');
 
 module.controller('newsController', CONTROLLER);
 
-function CONTROLLER($scope, $rootScope, $http, $localStorage, $location, jwtHelper, AuthenticationService) {
+function CONTROLLER($scope, $rootScope, News) {
     $scope.callback = function (code) {
-        console.log('  ----token 2 ' +code);
+        console.log('  ----token 2 ' + code);
+
+        News.GetNewsCode(code).then(function (data) {
+            console.log(JSON.stringify(data, null, "    "));
+            $scope.news = data;
+
+        });
+
+
     };
-
-
 
 
 };
