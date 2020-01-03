1080c2a91611952447b0c7216af978733204ace1
rowan <rowanfoo@gmail.com>
2020-01-03 15:22:53 +0800

    add falltoday and volumex

--- src/main/resources/static/user/useralgo.js
+++ src/main/resources/static/user/useralgo.js
@@ -7,7 +7,7 @@ function CONTROLLER($scope, $http, User, $localStorage) {
     $scope.ma = [];
     $scope.rsi = [];
     $scope.falltoday = [];
-
+    $scope.volumex = [];
     $scope.testuser = true;
 
     console.log("-----------useralgocontroller-------------");
@@ -28,6 +28,9 @@ function CONTROLLER($scope, $http, User, $localStorage) {
 
     $scope.newItem3 = function () {
         $scope.falltoday.push({id: 'falltoday', value: ''});
+    }
+    $scope.newItem4 = function () {
+        $scope.volumex.push({id: 'volumex', value: ''});
     }
 
 
@@ -71,13 +74,15 @@ function CONTROLLER($scope, $http, User, $localStorage) {
             var id = obj.id;
             if (id == 'ma') $scope.ma.push({id: obj.id, value: obj.value});
             else if (id == 'rsi') $scope.rsi.push({id: obj.id, value: obj.value});
+            else if (id == 'volumex') $scope.volumex.push({id: obj.id, value: obj.value});
+            else if (id == 'falltoday') $scope.falltoday.push({id: obj.id, value: obj.value});
         });
 //
 
         if ($scope.ma.length == 0) $scope.ma = [{id: 'ma', value: ''}];
         if ($scope.rsi.length == 0) $scope.rsi = [{id: 'rsi', value: ''}];
         if ($scope.falltoday.length == 0) $scope.falltoday = [{id: 'falltoday', value: ''}];
-
+        if ($scope.volumex.length == 0) $scope.volumex = [{id: 'volumex', value: ''}];
 
     });
 
