2b5e856682ef173735c832e02b4af57745640715
rowanfoo <rowanfoo@gmail.com>
2020-01-04 11:46:40 +0800

    add more algo

--- src/main/resources/static/app-service/user.js
+++ src/main/resources/static/app-service/user.js
@@ -2,9 +2,11 @@ angular
     .module('app')
     .factory('User', Service);
 
-function Service($http, $rootScope, $q) {
+function Service($http, $rootScope, $q, submitform) {
     var service = {};
     service.GetUserAlgo = getUserAlgo;
+    service.SaveUserAlgo = saveUserAlgo;
+    return service;
     return service;
 
     function getUserAlgo(user) {
@@ -15,15 +17,35 @@ function Service($http, $rootScope, $q) {
         $rootScope.algoscope = [];
         var wishlisturl = wishlisturl + 'user/algo/rowan';
         $http.get(wishlisturl).then(function (data) {
-            // console.log(JSON.stringify(data, null, "    "));
-            var mydata = data.data;
-            mydata.forEach(function (obj) {
-                algoscope.push({id: obj.id, value: obj.value});
-            });
-            q.resolve(algoscope);
-        });
+                console.log("------------------main------GetUserAlgo--------OK!!!---------------------");
+                // console.log(JSON.stringify(data, null, "    "));
+                var mydata = data.data;
+                mydata.forEach(function (obj) {
+                    algoscope.push({id: obj.id, value: obj.value});
+                });
+                q.resolve(algoscope);
+            }
+            , function (err) {
+                //  $scope.loading = false;
+                console.log("------------------main------GetUserAlgo--------ERRR----------123-----------");
+                q.reject(err);
+//                console.log(JSON.stringify(err, null, "    "));
+            }
+        );
         return q.promise;
     }
+
+
+    function saveUserAlgo(user) {
+
+        var userusrl = $rootScope.config.userurl + 'user/algo/rowan';
+        submitform.sendData('PUT', userusrl, user).then(function (data) {
+
+        });
+        console.log("----end ---");
+    }
+
+
 }
 
 
