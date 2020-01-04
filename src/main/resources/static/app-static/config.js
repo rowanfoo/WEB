ab6a764ea0141569fc075425afa4bff340dbb182
rowanfoo <rowanfoo@gmail.com>
2020-01-04 11:47:58 +0800

    add debug

--- src/main/resources/static/app-static/config.js
+++ src/main/resources/static/app-static/config.js
@@ -1,16 +1,16 @@
 var rooturl = 'http://rowanfoo.ddns.net';
 var serverurl = 'http://rowanfoo.ddns.net:10100/equity';
 var wishlisturl = 'http://rowanfoo.ddns.net:10100/wishlistserver';
+//var algoturl = 'http://localhost:8080/';
 var algoturl = 'http://rowanfoo.ddns.net:10100/';
 var oauthurl = 'http://rowanfoo.ddns.net:10200/';
 var userurl = 'http://rowanfoo.ddns.net:10300/';
+//var userurl = 'http://localhost:8090/';
 var wishurl = 'http://rowanfoo.ddns.net:10400/';
+//var wishurl = 'http://localhost:8045/';
 var configurl = 'http://rowanfoo.ddns.net:10500/config/';
-
-
 var commenturl = 'http://rowanfoo.ddns.net:10100/comment';
 var fundamentalurl = 'http://rowanfoo.ddns.net:10100/fundamental';
-
 var newsurl = 'http://rowanfoo.ddns.net:10900/news';
 
 function setconfig($rootScope) {
