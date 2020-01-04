eba67a922d184dc81de5295288ef43473acb3343
rowanfoo <rowanfoo@gmail.com>
2020-01-04 11:45:14 +0800

    add error

--- src/main/resources/static/wishlist/wishlistcreate/wishlistcreate.js
+++ src/main/resources/static/wishlist/wishlistcreate/wishlistcreate.js
@@ -23,18 +23,43 @@ function Controller($scope, $location, $http, submitform, $rootScope, WishList) 
         }
 
         console.log(JSON.stringify($scope.wishlist, null, "    "));
-        submitform.sendData('PUT', wishlisturl + '/wishlist/', $scope.wishlist);
+        submitform.sendData('PUT', wishlisturl + '/wishlist/', $scope.wishlist).then(function (data) {
+                $scope.error = "";
+            },
+            function (err) {
+
+                //               console.log("----error ---" + err);
+//                console.log(JSON.stringify(err, null, "    "));
+                console.log("----error ---" + err.data.code);
+                $scope.error = "these codes doesnt exists in database yet:" + err.data.code;
+            });
         console.log("----end ---");
+
+
+        // , function (err) {
+        //         $scope.loading = false;
+        //         console.log("------------------wishlistController------WISHLIST--------ERRR---------------------");
+        //         console.log(JSON.stringify(err, null, "    "));
+        //         $scope.error = "you are wrong ";
+        //
+        //     }
+        //
+        //
     };
 
     $scope.callback = function (selectedcategory) {
 
         WishList.GetWishCodes(selectedcategory).then(function (value) {
             console.log('HERE IN CONTROLLER ----------wishlistController---WISHLIST----RESULT-------***12*');
+            $scope.wishlist.category = selectedcategory;
             $scope.wishlist.code = value;
         });
+
 
     }
 
 
 };
+
+
+
