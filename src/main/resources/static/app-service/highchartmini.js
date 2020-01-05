b60a6a37206205a9bf7f7f0ab579779e28166dbf
rowanfoo <rowanfoo@gmail.com>
2020-01-05 11:04:53 +0800

    fix bug

--- src/main/resources/static/app-service/highchartmini.js
+++ src/main/resources/static/app-service/highchartmini.js
@@ -8,14 +8,15 @@ function Service($http, $localStorage) {
     return service;
 };
 
-function getMax(dataset) {
+function getMaxVal(dataset) {
     var max = Math.max.apply(Math, dataset.map(function (o) {
         return o[4];
     }));
-    return max + (max * 0.3);
+    console.log('--------------am i mad------max pure-' + max);
+    return max + (max * 0.1);
 }
 
-function getMin(dataset) {
+function getMinVal(dataset) {
     var min = Math.min.apply(Math, dataset.map(function (o) {
         return o[4];
     }));
@@ -30,7 +31,9 @@ function getMin(dataset) {
 function createChartx(seriesOptions, name) {
 
 // function createChart(seriesOptions, name, date) {
-    console.log('--------------am i mad-------')
+    console.log('--------------am i mad-1-----max-' + getMaxVal(seriesOptions));
+    console.log('--------------am i mad--1----min-' + getMinVal(seriesOptions));
+
     var date = new Date();
     // create the chart
     // chart = new  Highcharts.StockChart('container', {
@@ -45,7 +48,7 @@ function createChartx(seriesOptions, name) {
         },
 
         xAxis: {
-            min: Date.UTC(date.getFullYear(), 1, 1, 16, 00), //previous day  at 16.00
+            min: Date.UTC(date.getFullYear() - 1, 1, 1, 16, 00), //previous day  at 16.00
             max: new Date().getTime() //get actual time
         },
         scrollbar: {
@@ -65,23 +68,27 @@ function createChartx(seriesOptions, name) {
             split: true
         },
 
-        yAxis: [{
-
-            // title: {
-            //     text: 'OHLC'
-            // },
-            // top: 50,
-            // height: 400,
-            // opposite: false,
-            // min: getMin(seriesOptions),
-            // max: getMax(seriesOptions)
-            // min: 20,
-            // max: 50
-        }
-
-        ],
-
-
+        // yAxis: [{
+        //
+        //     // title: {
+        //     //     text: 'OHLC'
+        //     // },
+        //     // top: 50,
+        //     // height: 400,
+        //     // opposite: false,
+        //     min: 0.1,
+        //     max: 0.8
+        //     // min: 20,
+        //     // max: 50
+        // }
+        //
+        // ],
+        yAxis: {
+            min: getMinVal(seriesOptions),
+            //min: 0.2,
+            max: getMaxVal(seriesOptions)
+            // //max: 0.5
+        },
         series: [{
             type: 'ohlc',
             id: 'aapl',
