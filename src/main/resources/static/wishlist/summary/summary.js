c25c735994d8e1bac83306e28544aa57947af46d
rowanfoo <rowanfoo@gmail.com>
2020-02-06 21:44:30 +0800

    add wishlist summary

---
+++ src/main/resources/static/wishlist/summary/summary.js
@@ -0,0 +1,74 @@
+var module = angular.module('app');
+module.controller('wishlistSummaryController', Controller);
+
+
+function Controller($scope, $location, $http, $rootScope, $localStorage, DTOptionsBuilder, $mdDialog, CommentCreate) {
+    console.log('H------------------------------wishlistSummaryController------------------------------R');
+    $scope.loading = true;
+
+//////////////////////
+
+    console.log('H------------------------------wishlistSummaryController----------***USER--------------------R' + $localStorage.currentUser);
+
+    $http.get($rootScope.config.algoturl + 'wishlist/alldetails/' + $localStorage.currentUser).then(function (data) {
+        // console.log(JSON.stringify(data, null, "    "));
+        $scope.data = data.data
+        $scope.loading = false;
+    });
+    // $scope.dataTableOpt = {
+    //     //custom datatable options
+    //     // or load data through ajax call also
+    //     "aLengthMenu": [[20, 50, 100, -1], [20, 50, 100, 'All']],
+    // };
+
+    $scope.dataTableOpt = DTOptionsBuilder.newOptions();
+    $scope.dataTableOpt.withOption('createdRow', function (row, data, dataIndex) {
+
+        if (data[1].replace(/[\$,]/g, '') * 1 < 0) {
+            console.log('---dataTableOpt-------' + data[1])
+            $('td', row).eq(1).css('color', 'red');
+            $('td', row).eq(1).css('font-weight', 'bold');
+
+        }
+    })
+    // $scope.dataTableOpt.withOption("aLengthMenu", [10, 25, 50, 100]);
+    // $scope.dtOptions = DTOptionsBuilder.newOptions().withOption('lengthMenu', [10, 25, 50, 100])
+    $scope.dataTableOpt.withDisplayLength(20)
+    $scope.dataTableOpt.withOption('order', [[1, 'asc']])
+
+    ////////////////
+
+
+    $scope.chartdetail = function (code) {
+        $location.path("main/multichart/" + code);
+    };
+
+    $scope.list = function (code) {
+        $location.path('/main/listchart/' + code);
+    };
+
+    $scope.comment = function (code) {
+        $mdDialog.show({
+            //  templateUrl: "/WEB/static/dialog.html",
+            templateUrl: "/WEB/static/comment/dialog/commentDialog.html",
+            parent: angular.element(document.body),
+            clickOutsideToClose: true,
+            locals: {aTitle: code},
+            controller: function ($scope, $http, aTitle) {
+                CommentCreate.Createcontroller($scope, $http, aTitle, $mdDialog)
+            }
+        });
+    };
+
+    $scope.detail = function (code) {
+        $scope.mycode = code;
+        var userid = $localStorage.currentUser;
+        var url = 'main/viewdetail/' + code;
+        $location.path(url);
+    };
+
+
+    console.log('HERE IN CONTROLLER  DONE-1');
+
+
+};
