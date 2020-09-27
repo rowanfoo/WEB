6b9cd91f1cd2cfb9ca7379315db83c2455f906ae
rowanfoo <rowanfoo@gmail.com>
2020-09-27 12:22:58 +0800

    1. change spec , news is always a news of array

--- src/repo/model/WishlistSummary.ts
+++ src/repo/model/WishlistSummary.ts
@@ -1,9 +1,11 @@
+import {News} from "./News";
+
 export interface WishlistSummary {
   code: string,
   price: number,
   volume: number,
   message: string,
-  news: string,
+  news: News[],
   date: string
 }
 
