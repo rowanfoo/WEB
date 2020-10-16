67b68eb66aeba28b23365de18eca5a76d01db86b
rowanfoo <rowanfoo@gmail.com>
2020-10-16 09:07:57 +0800

    set date

--- src/app/share/toolbar/toolbar.component.ts
+++ src/app/share/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as moment from "moment";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-toolbar',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
+  date: String
 
-  constructor() { }
+  constructor(private  cookieService: CookieService) {
+  }
 
   ngOnInit() {
+    let mydate = this.cookieService.get('historydate')
+    this.date = (mydate) ? mydate : ''
+  }
+
+  EndDateChange(event) {
+
+    console.log('----EndDateChange----')
+    this.cookieService.set('historydate', moment(event.value).format('YYYY-MM-DD'), 0.02)
+    console.log('----EndDateChange--SET--' + this.cookieService.get('historydate'))
   }
 
 }
