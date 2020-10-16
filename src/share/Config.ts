017ed6196b9c13c53533b6436b01481b7c2851b0
rowanfoo <rowanfoo@gmail.com>
2020-10-16 11:18:37 +0800

    change config

--- src/share/Config.ts
+++ src/share/Config.ts
@@ -1,6 +1,6 @@
 export class Config {
-  // static rooturl = 'http://rowanfoo.ddns.net';
-  static rooturl = 'http://localhost';
+  static rooturl = 'http://rowanfoo.ddns.net';
+  //static rooturl = 'http://localhost';
 //  static rooturl = 'http://192.168.0.10';
 
   // static serverurl = Config.rooturl + ':10100/equity';
@@ -16,18 +16,19 @@ export class Config {
   // static newsurl = Config.rooturl + ':10900/news';
 
 
-  static algoturl = 'http://localhost:8080/';
-  static userurl = 'http://localhost:8090/';
-//  static wishurl = 'http://localhost:8045/';
-  static wishurl = 'http://localhost:8080/';
+  // static algoturl = 'http://localhost:8080/';
+  // static wishurl = 'http://localhost:8080/';
+  // static userurl = 'http://192.168.0.10:10300/';
 
+  static algoturl = Config.rooturl + ':10100/';
+  static wishurl = Config.rooturl + ':10100/';
+
+  static userurl = Config.rooturl + ':10300/';
   static oauthurl = Config.rooturl + ':10200/';
   static configurl = Config.rooturl + ':10500/config/';
   static commenturl = Config.rooturl + ':10100/comment';
   static categoryurl = Config.rooturl + ':10100/category';
   static fundamentalurl = Config.rooturl + ':10100/fundamental';
   static newsurl = Config.rooturl + ':10900/news';
-
-
 }
 
