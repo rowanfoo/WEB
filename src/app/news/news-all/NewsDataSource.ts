8c07a67fec7371096f3a6351959572c760560e68
rowanfoo <rowanfoo@gmail.com>
2020-10-16 09:19:23 +0800

    table usage

---
+++ src/app/news/news-all/NewsDataSource.ts
@@ -0,0 +1,38 @@
+import {DataSource} from "@angular/cdk/table";
+import {News} from "../../../repo/model/News";
+import {BehaviorSubject, Observable} from "rxjs";
+import {NewsRepo} from "../../../repo/repo/NewsRepo";
+import {CollectionViewer} from "@angular/cdk/collections";
+import {CookieService} from "ngx-cookie-service";
+
+export class NewsDataSourceDataSource implements DataSource<News> {
+
+  private todoSubject = new BehaviorSubject<News[]>([]);
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  private countSubject = new BehaviorSubject<number>(0);
+  public counter$ = this.countSubject.asObservable();
+
+  constructor(private  newsRepo: NewsRepo, private  cookieService: CookieService) {
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<News[]> {
+    return this.todoSubject.asObservable();
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.todoSubject.complete();
+    this.loadingSubject.complete();
+    this.countSubject.complete();
+  }
+
+
+  loadTodos(pageNumber = 0, pageSize = 35) {
+    this.loadingSubject.next(true);
+    let date = this.cookieService.get('historydate')
+    this.newsRepo.getAllNewsbyDate(date, {page: "0", size: "35"}).subscribe(value => {
+      this.todoSubject.next(value.content);
+      this.countSubject.next(value.totalElements);
+    })
+  }
+
+}
