d4723f02092ffd44b1fd12c995fc6533647481de
rowanfoo <rowanfoo@gmail.com>
2020-10-16 09:04:04 +0800

    change wording

--- src/repo/model/WishlistDetail.ts
+++ src/repo/model/WishlistDetail.ts
@@ -1,6 +1,6 @@
 // export class WishlistDetail{
-//  
-//  
+//
+//
 //   constructor(
 //
 //  code:string
@@ -16,22 +16,27 @@
 //
 //
 // ){
-//    
+//
 //   }
-//  
-//  
-//  
+//
+//
+//
 // }
 
 export interface WishlistDetail {
   code: string,
-  twenty: number,
-  fifty: number,
-  onehundred: number,
-  twohundred: number,
-  yearchange: number,
-  marketcap: number,
   pe: number,
   yield: number,
-  change: number
+  change: number,
+
+  percentage20: number,
+  maprice20: number,
+  percentage50: number,
+  maprice50: number,
+  percentage100: number,
+  maprice100: number,
+  percentage200: number,
+  maprice200: number
+
+
 }
