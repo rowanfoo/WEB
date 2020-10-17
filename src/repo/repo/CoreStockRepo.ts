d7d5f79c6be1d4def86b74da4d0ab4a908ef34ec
rowanfoo <rowanfoo@gmail.com>
2020-10-17 13:10:55 +0800

    save stock

--- src/repo/repo/CoreStockRepo.ts
+++ src/repo/repo/CoreStockRepo.ts
@@ -2,6 +2,10 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Config} from "../../share/Config";
 import {CoreStock} from "../model/CoreStock";
+import {map} from "rxjs/operators";
+import {Injectable} from "@angular/core";
+
+@Injectable()
 
 export class CoreStockRepo {
 
@@ -15,6 +19,26 @@ export class CoreStockRepo {
     return this.http.get<CoreStock[]>(url);
   }
 
+
+  getCode(code: string): Observable<CoreStock> {
+    var url = Config.algoturl + 'stocks/stock/' + code
+
+    console.log('---------getCode-------' + url);
+    return this.http.get<CoreStock>(url).pipe(
+      map(value => {
+        console.log(value);
+        return value
+      })
+    );
+  }
+
+  save(stock: CoreStock) {
+    var url = Config.algoturl + 'stocks';
+    return this.http.put<any>(url, stock);
+  }
+
+
 }
+
 
 
