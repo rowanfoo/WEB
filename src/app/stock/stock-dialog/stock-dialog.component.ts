195f6c985270f85531a1c0998bbd6073baab95fd
rowanfoo <rowanfoo@gmail.com>
2020-10-19 15:40:09 +0800

    add autocomplete for stock tags

--- src/app/stock/stock-dialog/stock-dialog.component.ts
+++ src/app/stock/stock-dialog/stock-dialog.component.ts
@@ -2,6 +2,9 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {CoreStockRepo} from "../../../repo/repo/CoreStockRepo";
 import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
 import {CoreStock} from "../../../repo/model/CoreStock";
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {map, startWith} from "rxjs/operators";
 
 @Component({
   selector: 'app-stock-dialog',
@@ -13,10 +16,22 @@ export class StockDialogComponent implements OnInit {
   code: string
   dialog: MatDialog
 
+  option: String[]
+  mycontrol = new FormControl()
+  filterOption: Observable<String[]>
+
+
   constructor(@Inject(MAT_DIALOG_DATA) data, private coreStockRepo: CoreStockRepo) {
     console.log('-----------StockDialogComponent------------------' + data.code)
     this.code = data.code
     this.dialog = data.dialog
+
+    this.coreStockRepo.getTags().subscribe(value => {
+      this.option = value
+      console.log(value)
+
+    })
+
   }
 
   ngOnInit() {
@@ -27,11 +42,32 @@ export class StockDialogComponent implements OnInit {
 
     })
 
+
+    this.filterOption = this.mycontrol.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    )
+
+
+  }
+
+  _filter(value: string): String[] {
+    console.log('------------FILTER---------' + value)
+    const filter = value.toLowerCase()
+    let x = this.option.filter(
+      value1 => {
+        console.log('------------VAL1---------' + value1 + '=')
+        return value1.toLowerCase().includes(filter)
+      }
+    )
+    return x
   }
 
   submitLogin() {
-    console.log('-----------StockDialogComponent-------SUBMIT-----------')
+    console.log('-----------StockDialogComponent-------SUBMIT---1--------')
     this.dialog.closeAll()
+    this.stock.tags = this.mycontrol.value
+
     this.coreStockRepo.save(this.stock).subscribe(
       (data) => {
         console.log('success----' + data);
@@ -39,7 +75,8 @@ export class StockDialogComponent implements OnInit {
       (error) => {
         console.log('success----' + error);
       }
-    );
+    )
+
   }
 
 
