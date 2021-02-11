c4a384ee5761bb0a533291d95f4386e60254e753
rowanfoo <rowanfoo@gmail.com>
2021-02-11 17:24:09 +0800

    save image

--- src/app/comment/comment-edit/comment-edit.component.ts
+++ src/app/comment/comment-edit/comment-edit.component.ts
@@ -1,7 +1,10 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {CommentRepo} from "../../../repo/repo/CommentRepo";
 import * as moment from "moment";
 import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
+import {ImageChart} from "../../../repo/model/ImageChart";
+import {Comments} from "../../../repo/model/Comment";
+import {ChartguiComponent} from "../../chart/chartgui/chartgui.component";
 
 //import {Comment} from "../../../repo/model/Comment";
 //import {ImageChart} from "./ImageChart";
@@ -22,7 +25,7 @@ export class CommentEditComponent implements OnInit {
     type: 'equity',
     reject: '',
     isReject: false,
-    image: null
+    image: new Array(1)
   }
 
   dialog: MatDialog
@@ -54,15 +57,57 @@ export class CommentEditComponent implements OnInit {
     this.toshow = true
   }
 
+  @ViewChild(ChartguiComponent) child: ChartguiComponent;
+
   submitLogin() {
     this.comment.userid = 'rowanf'
     console.log(this.comment);
     this.comment.date = moment().format('YYYY-MM-DD');
-    this.commentRepo.saveComment(this.comment).subscribe(value => {
+    if (this.toshow) {
+
+
+      // export interface ImageChart {
+      //   id: string,
+      //   date: string,
+      //   refid: string,
+      //   image: string,
+      // }
+      this.child.updateChart(this.saveimage);
+
+    } else {
+      this.save(this.comment)
+    }
+
+    this.dialog.closeAll()
+  }
+
+  private saveimage(data) {
+    console.log('--saveimage----1--------')
+    console.log(data)
+    let abc = new ImageChart(null, moment().format('YYYY-MM-DD'), null, data)
+    console.log('--saveimage----2--------')
+    //  this.comment.image = new Array()
+    console.log('--saveimage----2.1--------' + this.comment.image.length)
+    this.comment.image[0] = abc
+
+    //   this.comment.image.push(abc)
+    console.log('--saveimage----3--------')
+
+    //  this.save(this.comment)
+    console.log('-saveimage---4----------')
+
+  }
+
+  private save(comment: Comments) {
+    this.commentRepo.saveComment(comment).subscribe(value => {
+
       console.log('success  ' + value);
+      console.log(value.id);
+
     }, error1 => {
       console.log('errrr   ' + error1);
     })
-    this.dialog.closeAll()
+
   }
+
 }
