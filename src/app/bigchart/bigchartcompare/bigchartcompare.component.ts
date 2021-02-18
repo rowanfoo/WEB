b37c9ccd1807b681519ce8fd6a79acc835dfa508
rowanfoo <rowanfoo@gmail.com>
2021-02-18 19:05:25 +0800

    feature: componet can use input

--- src/app/bigchart/bigchartcompare/bigchartcompare.component.ts
+++ src/app/bigchart/bigchartcompare/bigchartcompare.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {BigChart} from "../../../repo/model/BigChart";
 import {BigChartRepo} from "../../../repo/repo/BigChartRepo";
 import {ActivatedRoute} from "@angular/router";
@@ -12,24 +12,34 @@ export class BigchartcompareComponent implements OnInit {
   bigcharts: BigChart
   bigchartcompare: BigChart[]
   width: string
-  load = false;
+  load = false
   toggle = true
+  @Input() year: string
+  @Input() mode: string
+  @Input() ma: string
+  @Input() code: string
 
 
   constructor(private bigChartRepo: BigChartRepo, private  route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.bigcharts = new BigChart()
     if (this.route.snapshot.params['code'] != null) {
-      this.bigcharts = new BigChart()
       this.bigcharts.year = this.route.snapshot.params['year'];
       this.bigcharts.mode = this.route.snapshot.params['mode'];
       this.bigcharts.ma = this.route.snapshot.params['ma'];
       this.bigcharts.code = this.route.snapshot.params['code'];
       this.width = "600"
       this.submitLogin()
+    } else if (this.code != null) {
+      this.bigcharts.year = this.year
+      this.bigcharts.mode = this.mode
+      this.bigcharts.ma = this.ma
+      this.bigcharts.code = this.code
+      this.width = "600"
+      this.submitLogin()
     } else {
-      this.bigcharts = new BigChart()
       this.bigcharts.year = "1"
       this.bigcharts.code = ""
       this.bigcharts.mode = "daily"
@@ -37,7 +47,6 @@ export class BigchartcompareComponent implements OnInit {
       this.width = "1200"
     }
   }
-
 
   submitLogin() {
     console.log(this.bigcharts)
