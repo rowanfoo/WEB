f0c3777e29515f393afd49df81f6b71acde952fb
rowanfoo <rowanfoo@gmail.com>
2021-03-03 22:10:44 +0800

    fix: bug url string return

--- src/etc/StringFunction.ts
+++ src/etc/StringFunction.ts
@@ -1,7 +1,7 @@
 export function fngetcodeonly(code: string): string {
 
   if (code.indexOf('.') > 0) {
-    code.substring(0, code.indexOf('.')).toUpperCase()
+    return code.substring(0, code.indexOf('.')).toUpperCase()
   } else {
     return code.toUpperCase()
   }
