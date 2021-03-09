324b79420f2ff6c237f07e8ccc8ab8ecff2ea4fa
rowanfoo <rowanfoo@gmail.com>
2021-03-09 19:38:33 +0800

    feature:mat-table and Paging

--- src/app/algo/algodatatable/algodatatable.component.ts
+++ src/app/algo/algodatatable/algodatatable.component.ts
@@ -1,9 +1,12 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {TechStr} from "../../../repo/model/TechStr";
 import {AlgoRepo} from "../../../repo/repo/AlgoRepo";
 import {ActivatedRoute} from "@angular/router";
-import {Subject} from "rxjs";
 import {CookieService} from "ngx-cookie-service";
+import {CommonFunction} from "../../../etc/CommonFunction";
+import {AlgoDataSourceDataSource} from "./AlgoDataSourceDataSource";
+import {MatPaginator} from "@angular/material/paginator";
+import {tap} from "rxjs/internal/operators/tap";
 
 @Component({
   selector: 'app-algodatatable',
@@ -15,97 +18,49 @@ import {CookieService} from "ngx-cookie-service";
 export class AlgodatatableComponent implements OnInit, OnDestroy {
   data: TechStr[]
 
-  constructor(private algo: AlgoRepo, private  route: ActivatedRoute, private  cookieService: CookieService) {
+  constructor(private algo: AlgoRepo, private  route: ActivatedRoute, private  cookieService: CookieService, private commonFunction: CommonFunction) {
   }
 
-  chartdetail(vode: string) {
-  }
-
-  comment(code: string) {
-  }
-
-  detail(code: string) {
-
-  }
-
-
-  marketcap: string
-  dtOptions: DataTables.Settings = {};
-  dtTrigger: Subject<any> = new Subject();
   codes: string
+  displayedColumns = ['code', 'name', 'type', 'message', 'chart', 'marketcap', 'indicator', 'news', 'date'];
+  todoDatasource: AlgoDataSourceDataSource;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngOnInit() {
-    console.log('------------------AlgodatatableComponent------------')
     let type = this.route.snapshot.params['type'];
-    let value = this.route.snapshot.params['value'];
-    let date = this.cookieService.get('historydate')
+    this.todoDatasource = new AlgoDataSourceDataSource(this.algo, this.commonFunction, this.callback, type)
+    this.todoDatasource.loadTodos();
+  }
 
-    console.log('------------------TableComponent------------' + type)
-    console.log('------------------TableComponent------------' + value)
-    console.log('------------------------AlgodatatableComponent-----------------------' + date)
+  callback(data: TechStr[]) {
+    let temp: string = ""
+    data.forEach(value1 => {
+      temp = temp + value1.code + ","
+    })
+    this.codes = temp.substring(0, temp.length - 1)
+  }
 
-
-//    this.algo.getData('rsi', '30<14')
-    //this.algo.getData(type, value)
-    this.algo.getAlgo(type, date)
-      .subscribe(
-        (data) => {
-
-          /// this.marketcap = data[0].fundamental.martketcapAsstring
-          //   let a = data[0].fundamental.martketcapAsstring
-          // let b = data[0].fundamental.marketcap
-          // let c = data[0].fundamental.shares
-          //
-          // console.log(this.marketcap);
-          // console.log(b);
-          // console.log(c);
-          // console.log(data[0].fundamental)
-          // console.log(data[0].fundamental.pe)
-          // console.log(data[0].fundamental.code)
-          // console.log(data[0].fundamental.date)
-          // console.log(data[0].fundamental.year_high_date)
-          // console.log(data[0].fundamental.market_cap)
-          // console.log(data[0].fundamental.yearchange)
-          // console.log(data[0].fundamental.annualYied)
-          // // data.forEach(value1 => {
-          //   console.log(value)
-          // })
-          console.log('------TEMP-------')
-          this.data = data
-          let temp: string = ""
-          data.forEach(value1 => {
-            temp = temp + value1.code + ","
-
-            //   console.log(temp)
-          })
-          this.codes = temp.substring(0, temp.length - 1)
-
-          this.dtTrigger.next()
-        }
+  ngAfterViewInit() {
+    this.todoDatasource.counter$
+      .pipe(
+        tap((count) => {
+          this.paginator.length = count;
+        })
       )
+      .subscribe();
 
-    // this.dtOptions = {
-    //   pagingType: 'full_numbers',
-    //   order: [[5, "desc"]]
-    // };
+    this.paginator.page
+      .pipe(
+        tap(() => this.loadTodos())
+      )
+      .subscribe();
+  }
 
-
-    this.dtOptions = {
-      pageLength: 20,
-      autoWidth: true,
-      pagingType: 'full_numbers',
-      order: [[4, 'desc']]
-    }
-
-
+  loadTodos() {
+    this.todoDatasource.loadTodos(this.paginator.pageIndex, this.paginator.pageSize);
   }
 
   ngOnDestroy(): void {
     // Do not forget to unsubscribe the event
-    this.dtTrigger.unsubscribe();
   }
-
 }
-
-
-
