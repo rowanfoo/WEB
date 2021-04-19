091abe45e2dc8e3e9746823f82492cfd95242b43
rowanfoo <rowanfoo@gmail.com>
2021-04-19 22:26:59 +0800

    fix appdisplay component codes

--- src/app/algo/algodatatable/algodatatable.component.ts
+++ src/app/algo/algodatatable/algodatatable.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {TechStr} from "../../../repo/model/TechStr";
 import {AlgoRepo} from "../../../repo/repo/AlgoRepo";
 import {ActivatedRoute} from "@angular/router";
@@ -7,6 +7,7 @@ import {CommonFunction} from "../../../etc/CommonFunction";
 import {AlgoDataSourceDataSource} from "./AlgoDataSourceDataSource";
 import {MatPaginator} from "@angular/material/paginator";
 import {tap} from "rxjs/internal/operators/tap";
+import {DisplaychartComponent} from "../../chart/displaychart/displaychart.component";
 
 @Component({
   selector: 'app-algodatatable',
@@ -15,29 +16,32 @@ import {tap} from "rxjs/internal/operators/tap";
 })
 
 
-export class AlgodatatableComponent implements OnInit, OnDestroy {
+export class AlgodatatableComponent implements OnInit, OnDestroy, AfterViewInit {
   data: TechStr[]
-
-  constructor(private algo: AlgoRepo, private  route: ActivatedRoute, private  cookieService: CookieService, private commonFunction: CommonFunction) {
-  }
-
+  @ViewChild(DisplaychartComponent) child: DisplaychartComponent;
   codes: string
   displayedColumns = ['code', 'name', 'type', 'message', 'chart', 'marketcap', 'indicator', 'news', 'date'];
   todoDatasource: AlgoDataSourceDataSource;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngOnInit() {
-    let type = this.route.snapshot.params['type'];
-    this.todoDatasource = new AlgoDataSourceDataSource(this.algo, this.commonFunction, this.callback, type)
-    this.todoDatasource.loadTodos();
+
+  constructor(private algo: AlgoRepo, private  route: ActivatedRoute, private  cookieService: CookieService, private commonFunction: CommonFunction) {
   }
 
-  callback(data: TechStr[]) {
-    let temp: string = ""
-    data.forEach(value1 => {
-      temp = temp + value1.code + ","
-    })
-    this.codes = temp.substring(0, temp.length - 1)
+  ngOnInit() {
+    let type = this.route.snapshot.params['type'];
+    this.todoDatasource = new AlgoDataSourceDataSource(this.algo, this.commonFunction,
+      (data: TechStr[]) => {
+        let temp: string = ""
+        data.forEach(value1 => {
+          temp = temp + value1.code + ","
+        })
+        this.codes = temp.substring(0, temp.length - 1)
+        console.log(this)
+        this.child.setcode(this.codes)
+      }
+      , type)
+    this.todoDatasource.loadTodos();
   }
 
   ngAfterViewInit() {
