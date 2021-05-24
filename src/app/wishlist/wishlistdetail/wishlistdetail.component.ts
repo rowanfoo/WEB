688004bc0a289dc746d64695f6f26d546c81df00
rowanfoo <rowanfoo@gmail.com>
2021-05-24 08:24:50 +0800

    wishlist details ..... add multichart random

--- src/app/wishlist/wishlistdetail/wishlistdetail.component.ts
+++ src/app/wishlist/wishlistdetail/wishlistdetail.component.ts
@@ -56,6 +56,9 @@ export class WishlistdetailComponent implements OnInit {
     this.router.navigate(['algo/bigchart/' + code + '/1'])
   }
 
+  private detail(code: string) {
+    this.router.navigate(["algo/bigchart/" + code + '/1']);
+  }
 }
 
 
